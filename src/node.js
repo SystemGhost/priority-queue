a91ec0ed40aa35ec498792907dee69aa61b2dcac
SystemGhost <jackie07364538@gmail.com>
2019-03-04 22:06:08 +0300

    feat: add solution for node

--- src/node.js
+++ src/node.js
@@ -1,23 +1,93 @@
 class Node {
-	constructor(data, priority) {
+  constructor(data, priority) {
+    this.data = data;
+    this.priority = priority;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
 
-	}
+  appendChild(node) {
+    if (this.left === null) {
+      this.left = node;
+      node.parent = this;
+    } else if (this.left !== null && this.right === null) {
+      this.right = node;
+      node.parent = this;
+    }
+  }
 
-	appendChild(node) {
+  removeChild(node) {
+    if (node.data === this.left.data && node.priority === this.left.priority) {
+      this.left.parent = null;
+      this.left = null;
+      return;
+    } else if (
+      node.data === this.right.data &&
+      node.priority === this.right.priority
+    ) {
+      this.right.parent = null;
+      this.right = null;
+      return;
+    }
 
-	}
+    throw new Error("not a child");
+  }
 
-	removeChild(node) {
+  remove() {
+    if (this.parent !== null) {
+      this.parent.removeChild(this);
+    }
+  }
 
-	}
+  swapWithParent() {
+    if (this.parent !== null) {
+      let childParent = null;
 
-	remove() {
+      let positionRight = false;
+      if (this.parent.left === this) {
+        childParent = this.parent.right;
+        positionRight = false;
+      } else if (this.parent.right === this) {
+        childParent = this.parent.left;
+        positionRight = true;
+      }
 
-	}
+      let overNode = this.parent.parent;
 
-	swapWithParent() {
-		
-	}
+      if (overNode !== null) {
+        if (overNode.left === this.parent) {
+          overNode.left = this;
+        } else if (overNode.right === this.parent) {
+          overNode.right = this;
+        }
+      }
+
+      this.parent.left = this.left;
+      this.parent.right = this.right;
+      this.parent.parent = this;
+      if (this.left !== null) {
+        this.left.parent = this.parent;
+      }
+      if (this.right !== null) {
+        this.right.parent = this.parent;
+      }
+
+      if (positionRight) {
+        this.right = this.parent;
+        this.left = childParent;
+      } else {
+        this.left = this.parent;
+        this.right = childParent;
+      }
+
+      if (childParent !== null) {
+        childParent.parent = this;
+      }
+
+      this.parent = overNode;
+    }
+  }
 }
 
 module.exports = Node;
